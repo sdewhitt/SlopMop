428d4178aad3261df46ee539e347900e35d2b62d
Raj Penmetcha <rajpenmetcha@pal-nat186-100-230.itap.purdue.edu>
2026-02-19 15:08:30 -0500

    added benefits section and placeholder installation steps

--- website/slopmop/app/page.tsx
+++ website/slopmop/app/page.tsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/navbar";
 import FeaturesSection from "./components/features-section";
 import Hero from "./components/hero";
+import PurposeSection from "./components/purpose-section";
+import InstallStepsSection from "./components/install-steps-section";
 
 export default function Home() {
   return (
@@ -10,6 +12,10 @@ export default function Home() {
         <Hero />
 
         <FeaturesSection />
+
+        <PurposeSection />
+
+        <InstallStepsSection />
     </div>
   );
 }
