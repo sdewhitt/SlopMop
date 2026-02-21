b543a11267211e06e99aecdb2c6ae76b65758c4f
sdewhitt <seth_dewhitt@outlook.com>
2026-02-21 02:31:25 -0500

    adjust website readme.md for unit testing info + new unit test for ensuring the navigation has correct hrefs

--- website/slopmop/app/__tests__/homepage.test.tsx
+++ website/slopmop/app/__tests__/homepage.test.tsx
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
 import Home from '../page'
 
 describe('Home', () => {
-  it('Renders the Navbar', () => {
+  it('Renders the Navbar & Title', () => {
     render(<Home />)
  
     const navbar = screen.getByRole('navigation')
@@ -11,5 +11,21 @@ describe('Home', () => {
  
     expect(navbar).toBeInTheDocument()
     expect(slopmopBrand).toBeInTheDocument()
+  })
+
+  it('Navigation links are present and have correct hrefs', () => {
+    render(<Home />)
+
+    const navbar = screen.getByRole('navigation')
+
+    const installLink = within(navbar).getByRole('link', { name: /Install/i })
+    const faqLink = within(navbar).getByRole('link', { name: /FAQ/i })
+    const signupLink = within(navbar).getByRole('link', { name: /Sign Up/i })
+    const homeLink = within(navbar).getByRole('link', { name: /SlopMop/i })
+
+    expect(installLink).toHaveAttribute('href', '/install')
+    expect(faqLink).toHaveAttribute('href', '/faq')
+    expect(signupLink).toHaveAttribute('href', '/signup')
+    expect(homeLink).toHaveAttribute('href', '/')
   })
 })
